99d9614ef143945cbb3f5cee9bd182a476606c73
vance <vancedowdell2110@yahoo.com>
2023-04-20 18:47:59 -0700

    new

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function checkValue() {
 	// Get the input value
 	var value = document.getElementById("input").value;
-	
+	let isFalsy =  value ==  "false"|| value === "" || value == "NaN" || value == "null" || value == 0  || value == "undefined";
 	// TODO: Check if the value is truthy or falsy
-	if ("ADD YOUR LOGIC EXPRESSION HERE"){
+	if (isFalsy){
 		document.getElementById("result").innerHTML = value + " is falsy!";
         document.getElementById("result").style.color = "red"
     } else {
